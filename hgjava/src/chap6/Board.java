cb4c9461d2d101b08758230c226dc6443c608805
vip <vip@DESKTOP-B327RD8>
2023-11-28 23:54:32 +0900

    23-11-28 숙제완료!

--- hgjava/src/chap6/Board.java
+++ hgjava/src/chap6/Board.java
@@ -1,13 +1,53 @@
 package chap6;
 
 public class Board {
-	String boardNo;
+	int boardNo;
 	String boardName;
 	String person;
 	String content;
 	String date;
 	
-	Board(String boardNo, String boardName, String person, String content, String date){
+	void showInfo() {
+		System.out.println(boardNo + ". " + boardName + " " + person);
+	}
+	void showDetail() {
+		System.out.println("번호: " + boardNo + " / 제목: " + boardName + "\n작성자: " + person + "\n내용: " + content + "\n일시: " + date);
+	}
+	
+	
+	void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	void setBoardName(String boardName) {
+		this.boardName = boardName;
+	}
+	void setPerson(String person) {
+		this.person = person;
+	}
+	void setContent(String content) {
+		this.content = content;
+	}
+	void setDate(String date) {
+		this.date = date;
+	}
+	
+	int getBoardNo() {
+		return boardNo;
+	}
+	String getBoardName() {
+		return boardName;
+	}
+	String getPerson() {
+		return person;
+	}
+	String getContent() {
+		return content;
+	}
+	String getDate() {
+		return date;
+	}
+
+	Board(int boardNo, String boardName, String person, String content, String date){
 		this.boardNo = boardNo;
 		this.boardName = boardName;
 		this.person = person;
@@ -15,5 +55,39 @@ public class Board {
 		this.date = date;
 	}
 	
+	
+	private Board[] boards;
+	
+	Board() {
+		boards = new Board[3];
+		
+		boards[0] = new Board(1,"자바게시판입니다","user01","오늘은 숙제로 자바게시판을 만들어봅시다.","2023-11-28");
+		boards[1] = new Board(2,"공부합니다","user02","오늘은 숙제로 자바게시판을 만들면서 공부합니다.","2023-11-28");
+	}
+	
+	
+
+	boolean addBoard(Board bod) {
+		for(int i = 0; i < boards.length; i++) {
+			if(boards[i] == null) {
+				boards[i] = bod;
+				return true; // 메소드에서 return문은 메소드의 끝.
+			}
+		}
+		return false;
+	}
+	
+	Board[] getBoardList() {
+		return boards;
+	}
+	
+	Board getBoard(int no) {
+		for(int i = 0; i < boards.length; i++) {
+			if(boards[i] != null && boards[i].getBoardNo() == no) {
+				return boards[i];
+			}
+		}
+		return null;
+	}
 	
 }//end of class
--- hgjava/src/chap6/BoardApp.java
+++ hgjava/src/chap6/BoardApp.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class BoardApp {
 
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
+//		Board[] boards = new Board[100];
 		boolean run = true;
-		Board[] boards = new Board[100];
-		System.out.println("1.등록 / 2.목록 / 3.상세조회 / 4.종료");
-		int menu = Integer.parseInt(scn.nextLine());
+		
+		Scanner scn = new Scanner(System.in);
+		Board exe1 = new Board();
 		while(run) {
+			System.out.println("1.등록 / 2.목록 / 3.상세조회 / 4.종료");
+			int menu = Integer.parseInt(scn.nextLine());
 			switch(menu) {
 			case 1 :
 				System.out.print("등록할 계시글번호 입력>> ");
-				String no = scn.nextLine();
+				int no = Integer.parseInt(scn.nextLine());
 				System.out.print("등록할 계시글제목 입력>> ");
 				String name = scn.nextLine();
 				System.out.print("계시글 작성자 입력>> ");
@@ -25,10 +27,44 @@ public class BoardApp {
 				String date = scn.nextLine();
 				
 				Board bod = new Board(no,name,person,content,date);
-				
-				
+				if(exe1.addBoard(bod)) {
+					System.out.println("게시글이 등록되었습니다.");
+				} else {
+					System.out.println("등록 실패!");
+				}
+				break;
+			case 2 :
+				for(Board boad : exe1.getBoardList()) {
+					if(boad != null) {
+						boad.showInfo();
+					}
+				}
+				break;
+			case 3 :
+//				번호: 1 / 제목: 자바게시판입니다
+//				작성자: user01
+//				내용: 오늘은 숙제로 자바게시판을 만들어봅시다.
+//				일시: 2023-11-28
+				System.out.println("상세조회할 게시글번호를 입력하세요.");
+				no = Integer.parseInt(scn.nextLine());
+//				for(int i = 0; i < boards.length; i++) {
+//					if(boards[i] != null && boards[i].getBoardNo() == no) {
+//						boards[i].showDetail();
+//					}
+//				}
+				Board bor = exe1.getBoard(no);
+				if(bor != null) {
+					bor.showDetail();
+				} else {
+					System.out.println("존재하지 않는 정보");
+				}
+				break;
+			case 4 :
+				System.out.println("프로그램을 종료합니다.");
+				run = false;
 			}//end of switch
 		}//end of while
+		System.out.println("end of prog");
 	}//end of main
 
 }//end of class
