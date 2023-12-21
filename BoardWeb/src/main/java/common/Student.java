dbd54ff0eb40391a314d1a4ae85c00a884374a28
admin <admin@DESKTOP-U2RH4QU>
2023-12-21 17:47:20 +0900

    2023-12-21

---
+++ BoardWeb/src/main/java/common/Student.java
@@ -0,0 +1,48 @@
+package common;
+
+public class Student {
+	private String studentNo; // student_no
+	private String studentName;
+	private int eng;
+	private int math;
+	Student(){
+		
+	}
+	
+	public void showInfo() {
+		System.out.println("이름은 " + studentName + "  영어점수는 " + eng + "  수학점수는 " + math + "입니다.");
+	}
+	
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	
+	public String getStudentNo() {
+		return studentNo;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public Student(String studentNo, String studentName,int eng,int math){
+		this.studentNo = studentNo;
+		this.studentName = studentName;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+}//end of class
