a5605a82db4e194d3939b319cc240f998edb621d
JeonSangJin <jsjini92@naver.com>
2024-01-04 10:49:09 +0900

    map, set 처리후

--- BoardWeb/src/main/webapp/javascript/js/class4.js
+++ BoardWeb/src/main/webapp/javascript/js/class4.js
@@ -11,11 +11,35 @@ const friend = {
 }
 friend.pets[0]['friends'][2] = '노량이';
 delete friend.age; // 속성을 지울땐 delete
-friend.ages = 20;
-friend.showFriends = function(){
+friend.ages = 20; // 속성추가
+friend.showFriends = function () { // 속성추가
     console.log(this);
 }
 console.log(friend);
 console.log(friend.pets[0]['friends']);
 
 // 참조값을 복사
+const fcopy = friend;  // 주소값을 복사.
+fcopy.age = 22; // 값을 변경하면 원본값도 같이 변함.
+console.log(fcopy);
+
+// 객체를 복사
+const fcopy2 = Object.assign({ bloodType: 'O' }, friend);  // assign : 새로운 객체를 생성
+fcopy2.age = 24; // 값을 변경해도 원본값은 유지됨.
+console.log(fcopy2);
+
+// 속성추가 : defineProperty()
+Object.defineProperty(friend, 'height', {
+    get: function () {
+        return this._height;
+    },
+    set: function (value) {
+        if (value <= 0) {
+            alert('error');
+        } else {
+            this._height = value; // _ : 숨김속성 / 이름이 같을때 사용하면됨.
+        }
+    }
+});
+friend.height = 162;
+console.log(friend.height);
--- BoardWeb/src/main/webapp/javascript/js/class5.js
+++ BoardWeb/src/main/webapp/javascript/js/class5.js
@@ -1,1 +1,62 @@
 // class5.js
+// Map, Set
+const map = new Map();
+map.set('홍길동', 20);
+map.set('김민수', 22);
+map.set('최성우', 25); // 추가
+map.set('홍길동', 21);
+
+map.delete('최성우');  // 삭제
+
+console.log(map.get('김민수')); // get(key) : key값의 value를 가져옴
+// map.clear();  // 모든요소 삭제
+
+for (let key of map.keys()) {
+    console.log(`key는 ${key}, value는 ${map.get(key)}`);
+}
+console.log(map.values());
+console.log(map.entries());
+for (let obj of map.entries()) {
+    console.log(obj);
+}
+for (let [item, value] of map.entries()) {
+    console.log(item, value);
+}
+
+// forEach는 첫번째 매개값이 value, 두번째 매개값이 key.
+map.forEach((value, key) => console.log(key, value));
+
+// Set
+const set = new Set();
+set.add('홍길동');
+set.add('김민수');
+set.add('홍성길');
+set.add('홍길동');
+
+set.delete('김민수');
+
+console.clear();
+console.log(set.size);
+
+set.forEach(name => console.log(name));
+
+const numAry = [10, 14, 12, 10]
+const num = new Set(numAry); // 배열 -> Set
+num.forEach(number => console.log(number));
+
+console.clear();
+map.clear();
+
+map.set('김명철', 80);
+map.set('김익수', 70);
+map.set('이명철', 80);
+map.set('이익수', 85);
+map.set('홍길동', 80);
+//김씨 점수의 합계
+let sum = 0;
+map.forEach((score, name) => {
+    if (name.indexOf('김') == 0) {
+        sum += score;
+    }
+});
+console.log(`합계점수: ${sum}`);
